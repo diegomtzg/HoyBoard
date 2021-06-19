0643952b03ad170c05b616e8d3059b55c2a2572d
Diego Martinez <diegomtzg96@gmail.com>
2021-06-18 18:43:40 -0600

    Email fetch finally works

--- src/components/Emails.js
+++ src/components/Emails.js
@@ -8,63 +8,77 @@ const fetchPeriod = 1000 * 60 * 5;
 export default function Emails() {
   const { signedIn } = useContext(AccountContext);
   const [emailIds, setEmailIds] = useState([]);
+  const [emails, setEmails] = useState({});
 
   useEffect(() => {
-    async function fetchUnreadEmails() {
+    function fetchUnreadEmailIds() {
       if (!signedIn) {
         return;
       }
 
-      console.log("Fetching emails...");
+      console.log("Fetching unread emails...");
       window.gapi.client.gmail.users.messages
         .list({
           userId: "me",
           labelIds: ["INBOX", "UNREAD"],
           maxResults: 10,
         })
-        .then(function (response) {
-          setEmailIds(response.result.messages.map((message) => message.id));
+        .then((listResponse) => {
+          const ids = listResponse.result.messages.map((message) => message.id);
+          setEmailIds(ids);
         });
+
+      emailIds.forEach((id) => {
+        // We have ids for each email so we need to get additional information.
+        window.gapi.client.gmail.users.messages
+          .get({
+            userId: "me",
+            id: id,
+          })
+          .then((detailResponse) => {
+            var date = detailResponse.result.internalDate;
+            var snippet = detailResponse.result.snippet.replace(/&#39;/g, "'");
+            var subject, sender;
+            detailResponse.result.payload.headers.forEach((header) => {
+              if (header.name === "Subject" || header.name === "subject") {
+                subject = header.value;
+              }
+              if (header.name === "From" || header.name === "from") {
+                try {
+                  [, sender] = /"?([^"\n]*)"? <.*>/.exec(header.value);
+                } catch (error) {
+                  sender = header.value;
+                }
+              }
+            });
+            if (subject === undefined) {
+              subject = "(no subject)";
+            }
+            const email = {
+              sender: sender,
+              subject: subject,
+              snippet: snippet,
+              date: date,
+            };
+
+            var currEmails = emails;
+            currEmails[id] = email;
+            setEmails(currEmails);
+          });
+      });
     }
 
-    fetchUnreadEmails();
+    fetchUnreadEmailIds();
     // const interval = setInterval(fetchUnreadEmails, fetchPeriod);
     // return () => clearInterval(interval);
   }, [signedIn]);
-
-  function renderEmail(emailId) {
-    window.gapi.client.gmail.users.messages
-      .get({
-        userId: "me",
-        id: emailId,
-      })
-      .then((response) => {
-        var snippet = response.result.snippet.replace(/&#39;/g, "'");
-        var subject, sender;
-        response.result.payload.headers.forEach((header) => {
-          if (header.name === "Subject" || header.name === "subject") {
-            subject = header.value;
-          }
-          if (header.name === "From" || header.name === "from") {
-            [, sender] = /"?([^"\n]*)"? <.*>/.exec(header.value);
-          }
-        });
-
-        console.log(sender);
-        return (
-          <p>
-            {sender} – {subject}
-          </p>
-        );
-      });
-  }
 
   return (
     <div className="emails">
       <h1>Emails</h1>
       <ul>
-        {emailIds.map((emailId, idx) => (
-          <li key={idx}>{emailId}</li>
+        {emailIds.map((id, idx) => (
+          <li key={idx}>{emails[id].sender}</li>
         ))}
       </ul>
     </div>
