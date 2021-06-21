9eca1b42c4f32ddeacdaa0d8ad343adc9e3c4dc3
Diego Martinez <diegomtzg96@gmail.com>
2021-06-20 18:16:47 -0600

    Change news source to NYT and clean up interface

--- src/components/News.js
+++ src/components/News.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import PulseLoader from "react-spinners/PulseLoader";
 import "../static/css/news.css";
 
-// Once per hour
-const fetchPeriod = 1000 * 60 * 60;
-const numResults = 8;
-const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+// Once every 30 mins
+const fetchNewsPeriod = 1000 * 60 * 30;
+const shownResults = 4;
+const NEWS_API_KEY = process.env.REACT_APP_NYT_API_KEY;
 
 export default function News() {
   const [news, setNews] = useState([]);
@@ -14,28 +14,24 @@ export default function News() {
   useEffect(() => {
     async function fetchNews() {
       console.log("Fetching news...");
-      var reqUrl =
-        "https://newsapi.org/v2/top-headlines?" +
-        "country=us&" +
-        `apiKey=${NEWS_API_KEY}&` +
-        `category=general&` +
-        `pageSize=${numResults}`;
+      const reqUrl =
+        `https://api.nytimes.com/svc/topstories/v2/technology.json?` +
+        `api-key=${NEWS_API_KEY}`;
       const response = await fetch(reqUrl);
       const news = await response.json();
-      const cleanedNews = news.articles.map((article) => {
-        let [, title, source] = /(.*) - (.+)/.exec(article.title);
-        return { title: title, source: source };
-      });
-      console.log(cleanedNews);
-      setNews(cleanedNews);
+      console.log(news.results);
+      setNews(news.results);
       setLoading(false);
     }
 
     fetchNews();
-    const interval = setInterval(fetchNews, fetchPeriod);
-    return () => clearInterval(interval);
+    const fetchNewsInterval = setInterval(fetchNews, fetchNewsPeriod);
+    return () => {
+      clearInterval(fetchNewsInterval);
+    };
   }, []); // Run only when component mounts, not when state changes (state set here, runs forever)
 
+  // TODO: Rotate between all available headlines (not just the first few)
   if (loading) {
     return <PulseLoader color={"#8f8f8f"} loading={loading} />;
   } else {
@@ -43,19 +39,11 @@ export default function News() {
       <div className="news">
         <h1 className="news-title">Today's Headlines</h1>
         <ul className="news-list">
-          <div className="news-headline">
-            <li key={0}>
-              <span className="news-source">{`${news[0].source} – `}</span>
-              {news[0].title}
+          {news.slice(0, shownResults).map((article, idx) => (
+            <li key={idx}>
+              <span className="news-headline">{article.title}</span>
+              <p className="news-abstract">{article.abstract}</p>
             </li>
-          </div>
-          {news.slice(1).map((article, idx) => (
-            <div className="news-headline top-border">
-              <li key={idx}>
-                <span className="news-source">{`${article.source} – `}</span>
-                {article.title}
-              </li>
-            </div>
           ))}
         </ul>
       </div>
