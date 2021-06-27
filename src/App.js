6022d7512b177ed4718929f51393497b6451cd52
Diego Martinez <diegomtzg96@gmail.com>
2021-06-27 16:38:27 -0600

    Redeploy with updated env variables

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       window.gapi.client
         .init({
           apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-          clientId: process.env.REACT_APP_GCAL_CLIENT_ID,
+          clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           discoveryDocs: [
             "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
             "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
